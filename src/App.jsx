29399807b83f2a839f4f0476c40f825eb4d821e8
NazaRamos <nazareno.94@hotmail.com>
2023-08-28 11:58:22 -0300

    Entrega M03-LAYOUT

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Body from './components/Body'
+import DivBody from './components/DivBody'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Body/>
+    <DivBody/>
   )
 }
 
