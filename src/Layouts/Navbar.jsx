40b44ecc29c30c7616aee7c3373129790f32ae6e
NazaRamos <139928694+NazaRamos@users.noreply.github.com>
2023-08-29 18:35:06 -0300

    Entrega M04-NAVBAR (#4)
    * Entrega M01-STATIC
    
    * Entrega M02-COMPOSITION
    
    * Entrega M03-LAYOUT
    
    * Entrega M04-NAVBAR

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Display from '../components/Display'
 
-export default function Navbar() {
+const Navbar = () => {
+  let [open, setOpen] = useState(false)
+  
   return (
-    <nav className='flex absolute justify-between items-center px-4 pt-2 h-1/6 md:absolute w-full'>
-        <img className='md:hidden w-15 h-12 absolute top-5 left-5' src="../public/images/Menu_mobile.png" alt="" />
-        <img className='max-md:hidden w-15 h-12 absolute top-5 left-5' src="../public/images/Menu.png" alt="" />
-        <img className="w-15 h-8 absolute top-5 right-5" src="../public/images/Logo(1)_3.png" alt="" />
-    </nav>
+    <>
+      <nav className='flex absolute justify-between items-center px-4 pt-2 h-1/6 md:absolute w-full'>
+          <img onClick={() => setOpen(true)} className='md:hidden w-15 h-12 absolute top-5 left-5 cursor-pointer' src="../public/images/Menu_mobile.png" alt="" />
+          <img onClick={() => setOpen(true)} className='max-md:hidden w-15 h-12 absolute top-5 left-5 cursor-pointer' src="../public/images/Menu.png" alt="" />
+          <img className="w-15 h-8 absolute top-5 right-5" src="../public/images/Logo(1)_3.png" alt="" />
+      </nav>
+      {open && <Display openClose={setOpen} />}
+    </>
   )
 }
+
+export default Navbar
